53a0ac78c7734beba8933e47d56527355c26fe37
Ereizan <ereizan@gmail.com>
2022-09-27 01:23:08 +0700

    Just...

--- src/matrixSpiral.java
+++ src/matrixSpiral.java
@@ -25,47 +25,40 @@ import java.util.NoSuchElementException;
 //Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
 public class matrixSpiral {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+        int[] solution;
+        int[][] matrix;
 
-            int[] solution;
-            String line = "1";
-            ArrayList<String> stringLine = new ArrayList<>();
-
-            System.out.println ("Put a matrix");
-            Scanner scan = new Scanner (System.in);
-      /*
-      try {
-          while (!line.isEmpty()){
-          line = scan.nextLine();
-          stringLine.add(line);
-          scan = new Scanner (System.in);
-          }
-      } catch (NoSuchElementException e){      }
-
-     */
-
-
-            // matrix = turnToIntArray(stringLine);
-            int[][] matrix =  {{1, 2, 3, 8, 1},
-                    {3, 2, 1, 5, 6},
-                    {5, 1, 2, 9, 8},
-                    {4, 5, 6, 2, 1},
-                    {3, 5, 4, 6, 1}};
-            for (int[] lineable : matrix){
-                System.out.println (Arrays.toString(lineable));
+        String line;
+        ArrayList<String> stringLine = new ArrayList<>();
+        System.out.println ("Put a matrix");
+        Scanner scan = new Scanner (System.in);
+        while (true){
+            try {
+                line = scan.nextLine();
+            } catch (NoSuchElementException e){
+                line = "";
             }
-            System.out.println ();
-            solution = getSolution(matrix);
-            System.out.println (Arrays.toString(solution));
-
+            if (line.isEmpty()) break;
+            stringLine.add(line);
+            scan = new Scanner (System.in);
         }
 
+        matrix = turnToIntArray(stringLine);
+        for (int[] matrixLine : matrix){
+            System.out.println (Arrays.toString(matrixLine));
+        }
+        System.out.println ();
+        solution = getSolution(matrix);
+        System.out.println (Arrays.toString(solution));
+    }
+
         static int[][] turnToIntArray (ArrayList<String> stringLine){
-            String[] chek = stringLine.get(0).split(" ");
-            int[][] intArray = new int[stringLine.size()][chek.length];
+            String[] check = stringLine.get(0).split(" ");
+            int[][] intArray = new int[stringLine.size()][check.length];
             int count = 0;
             for (String line : stringLine){
-                String[] strings = stringLine.get(0).split(" ");
+                String[] strings = line.split(" ");
                 int[] mod = new int[strings.length];
                 int number = 0;
                 for (String string : strings){
@@ -76,15 +69,15 @@ public class matrixSpiral {
             return intArray;
         }
         static int[] getSolution (int[][] matrix){
-            int lenghtSolution = matrix.length * matrix[0].length;
-            int[] solution = new int[lenghtSolution];
+            int lengthSolution = matrix.length * matrix[0].length;
+            int[] solution = new int[lengthSolution];
             int counterX = matrix[0].length - 1;
             int counterY = matrix.length - 1;
             int taperX = 0;
             int taperY = 0;
             int vector = 1;
 
-            for (int i = 0; i < lenghtSolution; i++){
+            for (int i = 0; i < lengthSolution; i++){
                 solution[i] = matrix[taperY][taperX];
                 switch (vector) {
                     case 1 :
