2a2738bd3d7f03c4b82c7b64c749f152a3472a5d
Ereizan <ereizan@gmail.com>
2022-10-10 00:45:03 +0700

    some changes

--- src/SelfAvoidingWalk.java
+++ src/SelfAvoidingWalk.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 /*
@@ -12,6 +13,7 @@ public class SelfAvoidingWalk {
     static boolean deadEnd = false;
     static boolean winEnd = false;
     static Random random = new Random(System.nanoTime());
+    static ArrayList<Integer[]> path = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("Set length of Area");
@@ -23,7 +25,7 @@ public class SelfAvoidingWalk {
         if (winEnd){
             System.out.println("It's escape!");
         }
-        paintArea();
+        getGraph();
     }
     public static void areaInitialization(int areaLength){
         area = new boolean[areaLength][areaLength];
@@ -36,6 +38,7 @@ public class SelfAvoidingWalk {
         x = areaLength / 2;
     }
     public static void move(){
+        addPath(y, x);
         while (true){
             area[y][x] = true;
             int a = x;
@@ -52,6 +55,7 @@ public class SelfAvoidingWalk {
             if (x == 0 || y == 0 || x == area.length - 1 || y == area.length - 1){
                 winEnd = true;
                 area[y][x] = true;
+                addPath(y, x);
                 break;
             }
             if (area[b + 1][a] && area[b - 1][a] && area[b][a + 1] && area [b][a - 1]) {
@@ -61,21 +65,65 @@ public class SelfAvoidingWalk {
             if (area[y][x]){
                 x = a;
                 y = b;
+                continue;
             }
+            addPath(y, x);
         }
     }
+    public static void addPath(int y, int x){
+        Integer[] step = {y, x};
+        path.add(step);
+    }
 
-    public static void paintArea(){
+    public static void paintArea(boolean[][] area){
         System.out.println();
         for (boolean[] booleans : area){
             for (boolean cell : booleans){
                 if (cell) {
-                    System.out.print("+");
-                } else {
                     System.out.print("o");
+                } else {
+                    System.out.print(" ");
                 }
             }
             System.out.println();
         }
+    }
+    static void getGraph(){
+        int graphLength = area.length * 3;
+        boolean[][] graph = new boolean[graphLength][graphLength];
+        for (int i = 0; i < graphLength; i++){
+            for (int j = 0; j < graphLength; j++){
+                graph[i][j] = false;
+            }
+        }
+        for (int n = 0; n < graphLength; n++){
+            graph[n][0] = true;
+            graph[n][graphLength - 1] = true;
+            graph[0][n]= true;
+            graph[graphLength - 1][n] = true;
+        }
+        for (int m = 0; m < path.size(); m++){
+            int y = path.get(m)[0] * 3 + 1;
+            int x = path.get(m)[1] * 3 + 1;
+            graph[y][x] = true;
+            if (m == 0) continue;
+            if (path.get(m)[0] < path.get(m - 1)[0]){
+                graph[y+1][x] = true;
+                graph[y+2][x] = true;
+            }
+            if (path.get(m)[0] > path.get(m - 1)[0]){
+                graph[y-1][x] = true;
+                graph[y-2][x] = true;
+            }
+            if (path.get(m)[1] < path.get(m - 1)[1]){
+                graph[y][x+1] = true;
+                graph[y][x+2] = true;
+            }
+            if (path.get(m)[1] > path.get(m - 1)[1]){
+                graph[y][x-1] = true;
+                graph[y][x-2] = true;
+            }
+        }
+        paintArea(graph);
     }
 }
